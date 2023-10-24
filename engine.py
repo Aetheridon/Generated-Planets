3408f6e246ea509a4aa353d88ff245fafb2c91ba
Aetheridon <lewisvr@protonmail.com>
2023-10-24 20:31:56 +0100

    Moved planet generation into Class

--- engine.py
+++ engine.py
@@ -1,10 +1,26 @@
+import random
+
 from ursina import Ursina, Entity, color
 
 app = Ursina()
-sphere = Entity(model="sphere", color=color.red, texture="white_cube", scale=2)
+
+class Planet():
+    def __init__(self):
+        self.model = "sphere"
+        self.color = color.red
+        self.texture = "white_cube"
+        self.scale = random.randint(1, 5)
+    
+    def generate_planet(self):
+        planet = Entity(model=self.model, color=self.color, texture=self.texture, scale=self.scale)
+        return planet
+
+planet_obj = Planet()
+print(f"scale: {planet_obj.scale}")
+generated_planet = planet_obj.generate_planet()
 
 def update():
-    sphere.rotation_x = sphere.rotation_x + 0.25
-    sphere.rotation_y = sphere.rotation_y + 0.5
+    generated_planet.rotation_x = generated_planet.rotation_x + 0.25
+    generated_planet.rotation_y = generated_planet.rotation_y + 0.5
 
 app.run()
